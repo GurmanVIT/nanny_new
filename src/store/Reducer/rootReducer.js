70f1d059992b39a5b4d2357ab80e840ba64e8826
AnmolDang <anmoldang121@gmail.com>
2024-02-08 20:09:37 +0530

    delete funcationality

--- src/store/Reducer/rootReducer.js
+++ src/store/Reducer/rootReducer.js
@@ -1,64 +1,64 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers } from "@reduxjs/toolkit";
 //import authReducer from './path/to/authSlice';
-import loginReducer from '../apiSlice/LoginSlice'
-import signupReducer from '../apiSlice/Signupnannyslice';
-import  otpReducer from '../apiSlice/Otpslice';
-import cardsReducer from '../apiSlice/NcategoriesSlice';
-import ForgotpasdReducer from '../apiSlice/ForgotpasswordSlice';
-import signUpfamilyReducer from '../apiSlice/SignupfamilySilce';
-import NlistReducer from '../apiSlice/NanylistSlice';
-import bookNannyReducer from '../apiSlice/BooknannySlice';
-import EarningReducer from '../apiSlice/EarningSlice';
-import ongoingReducer from '../apiSlice/OngoingSlice';
-import upcommingReducer from '../apiSlice/UpcommingSlice';
-import nannyProfileReducer from '../apiSlice/NannyprofileSlice';
-import NannyEditprofileReducer from '../apiSlice/NeditProfileSlice';
-import UserProfileReducer from '../apiSlice/UserprofileSlice';
-import changePasswordReducer from '../apiSlice/ChangePasswordSlice';
-import UserEditprofileReducer from '../apiSlice/EditUserProfileSlice';
-import NannyupcommingReducer from '../apiSlice/NannyUpcomingSlice';
-import NannyOngoingReducer from '../apiSlice/OngoingNannySlice';
-import NannyPastReducer from '../apiSlice/NannypastSlice';
-import UpdateBookingStatusReducer from '../apiSlice/UpdateBookingStatusSlice';
-import getAllCategoriesReducer from '../apiSlice/ManageservicesSlice';
-import DocumentsnannyReducer from '../apiSlice/DocumentsnannySlice';
-import uploadFileReducer from '../apiSlice/uploadFileSlice';
-import uploadDocumentsReducer from '../apiSlice/UpdateDocumentSlice';
-import updateNannyCatgoriesReducer from '../apiSlice/UpdateNannyCatgoriesSlice';
-import getTiminglistReducer from '../apiSlice/GetTimingSlice';
-import updateTimingslistReducer from '../apiSlice/UpdateTimingsSlice';
-
+import loginReducer from "../apiSlice/LoginSlice";
+import signupReducer from "../apiSlice/Signupnannyslice";
+import otpReducer from "../apiSlice/Otpslice";
+import cardsReducer from "../apiSlice/NcategoriesSlice";
+import ForgotpasdReducer from "../apiSlice/ForgotpasswordSlice";
+import signUpfamilyReducer from "../apiSlice/SignupfamilySilce";
+import NlistReducer from "../apiSlice/NanylistSlice";
+import bookNannyReducer from "../apiSlice/BooknannySlice";
+import EarningReducer from "../apiSlice/EarningSlice";
+import ongoingReducer from "../apiSlice/OngoingSlice";
+import upcommingReducer from "../apiSlice/UpcommingSlice";
+import nannyProfileReducer from "../apiSlice/NannyprofileSlice";
+import NannyEditprofileReducer from "../apiSlice/NeditProfileSlice";
+import UserProfileReducer from "../apiSlice/UserprofileSlice";
+import changePasswordReducer from "../apiSlice/ChangePasswordSlice";
+import UserEditprofileReducer from "../apiSlice/EditUserProfileSlice";
+import NannyupcommingReducer from "../apiSlice/NannyUpcomingSlice";
+import NannyOngoingReducer from "../apiSlice/OngoingNannySlice";
+import NannyPastReducer from "../apiSlice/NannypastSlice";
+import UpdateBookingStatusReducer from "../apiSlice/UpdateBookingStatusSlice";
+import getAllCategoriesReducer from "../apiSlice/ManageservicesSlice";
+import DocumentsnannyReducer from "../apiSlice/DocumentsnannySlice";
+import uploadFileReducer from "../apiSlice/uploadFileSlice";
+import uploadDocumentsReducer from "../apiSlice/UpdateDocumentSlice";
+import updateNannyCatgoriesReducer from "../apiSlice/UpdateNannyCatgoriesSlice";
+import getTiminglistReducer from "../apiSlice/GetTimingSlice";
+import updateTimingslistReducer from "../apiSlice/UpdateTimingsSlice";
+import deleteTimingsReducer from "../apiSlice/DeleteTimingsSlice";
 
 const rootReducer = combineReducers({
   login: loginReducer,
-  signUp:signupReducer,
+  signUp: signupReducer,
   otp: otpReducer,
-  cards:cardsReducer,
-  Forgotpasd:ForgotpasdReducer,
-  signUpfamily:signUpfamilyReducer,
-  Nlist:NlistReducer,
-  bookNanny:bookNannyReducer,
-  Earning:EarningReducer,
-  ongoingReducer:ongoingReducer,
-  upcommingReducer:upcommingReducer,
-  nannyProfileReducer:nannyProfileReducer,
-  NannyEditprofileReducer:NannyEditprofileReducer,
-  UserProfileReducer:UserProfileReducer,
-  changePasswordReducer:changePasswordReducer,
-  UserEditprofileReducer:UserEditprofileReducer,
-  NannyupcommingReducer:NannyupcommingReducer,
-  NannyOngoingReducer:NannyOngoingReducer,
-  NannyPastReducer:NannyPastReducer,
-  UpdateBookingStatusReducer:UpdateBookingStatusReducer,
-  getAllCategoriesReducer:getAllCategoriesReducer,
-  DocumentsnannyReducer:DocumentsnannyReducer,
-  uploadFileReducer:uploadFileReducer,
-  uploadDocumentsReducer:uploadDocumentsReducer,
-  updateNannyCatgoriesReducer:updateNannyCatgoriesReducer,
-  getTiminglistReducer:getTiminglistReducer,
-  updateTimingslistReducer:updateTimingslistReducer,
+  cards: cardsReducer,
+  Forgotpasd: ForgotpasdReducer,
+  signUpfamily: signUpfamilyReducer,
+  Nlist: NlistReducer,
+  bookNanny: bookNannyReducer,
+  Earning: EarningReducer,
+  ongoingReducer: ongoingReducer,
+  upcommingReducer: upcommingReducer,
+  nannyProfileReducer: nannyProfileReducer,
+  NannyEditprofileReducer: NannyEditprofileReducer,
+  UserProfileReducer: UserProfileReducer,
+  changePasswordReducer: changePasswordReducer,
+  UserEditprofileReducer: UserEditprofileReducer,
+  NannyupcommingReducer: NannyupcommingReducer,
+  NannyOngoingReducer: NannyOngoingReducer,
+  NannyPastReducer: NannyPastReducer,
+  UpdateBookingStatusReducer: UpdateBookingStatusReducer,
+  getAllCategoriesReducer: getAllCategoriesReducer,
+  DocumentsnannyReducer: DocumentsnannyReducer,
+  uploadFileReducer: uploadFileReducer,
+  uploadDocumentsReducer: uploadDocumentsReducer,
+  updateNannyCatgoriesReducer: updateNannyCatgoriesReducer,
+  getTiminglistReducer: getTiminglistReducer,
+  updateTimingslistReducer: updateTimingslistReducer,
+  deleteTimingsReducer: deleteTimingsReducer,
 
-  
   // Add other reducers as needed
 });
 
--- src/store/apiSlice/DeleteTimingsSlice.js
+++ src/store/apiSlice/DeleteTimingsSlice.js
@@ -1,50 +1,58 @@
-
-
 // src/redux/cardsSlice.js
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { BASE_URL, deleteTimingsApi } from '../../utils/Constants';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BASE_URL, deleteTimingsApi } from "../../utils/Constants";
 
 // Async thunk for fetching data
-export const deleteTimingslist = createAsyncThunk('deleteTimingslist', async (payload) => {
-  const token = localStorage.getItem("Token")
-  const headers = {
-    "Authorization": token, "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
+export const deleteTimingslist = createAsyncThunk(
+  "deleteTimingslist",
+  async (payload) => {
+    const token = localStorage.getItem("Token");
+    const headers = {
+      Authorization: token,
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    };
+    try {
+      const url = BASE_URL + deleteTimingsApi;
+      const response = await axios.put(url, payload, { headers });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
-  try {
-    const url = BASE_URL + deleteTimingsApi 
-    const response = await axios.put(url , payload, { headers }); 
-    console.log(response.data)
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-});
+);
 
 const deleteTimingsSlice = createSlice({
-  name: 'deleteTimingsReducer',
+  name: "deleteTimingsReducer",
   initialState: {
     data: null,
-    loading: 'idle',
+    loading: "idle",
     error: null,
   },
-  reducers: {},
+  reducers: {
+    clearDeleteData: (state) => {
+      // Reset the data property to an empty array
+      state.data = null;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(deleteTimingslist.pending, (state) => {
-        state.loading = 'pending';
+        state.loading = "pending";
       })
       .addCase(deleteTimingslist.fulfilled, (state, action) => {
-        state.loading = 'idle';
+        state.loading = "idle";
         state.data = action.payload;
         state.error = null;
       })
       .addCase(deleteTimingslist.rejected, (state, action) => {
-        state.loading = 'idle';
+        state.loading = "idle";
         state.error = action.error.message;
       });
   },
 });
 
+export const { clearDeleteData } = deleteTimingsSlice.actions;
 export default deleteTimingsSlice.reducer;
